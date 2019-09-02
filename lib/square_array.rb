cb75114912af651a2bf64532f42366570d1a3e56
Brian Kao <bk1613@nyu.edu>
2019-09-02 20:19:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   newarray = []
   while counter < array.length do
-    newarray.push(array[counter]**2)
+    newarray.push(array[counter] * array[counter])
     puts newarray
     counter += 1
   end
